4298201548de24c9577b3e3a0626446b5d91f1ae
Peter Witham <peter.witham@gmail.com>
2023-05-28 22:30:57 -0500

    images are now removed from the share array when deselected

--- TransferableTest/Models/ImageModel.swift
+++ TransferableTest/Models/ImageModel.swift
@@ -8,7 +8,7 @@
 import Foundation
 import SwiftUI
 
-struct ImageModel: Hashable {
+struct ImageModel: Hashable, Identifiable {
   var id: UUID
   var imageFile: Image
 
--- TransferableTest/Views/GridImageItem.swift
+++ TransferableTest/Views/GridImageItem.swift
@@ -14,34 +14,37 @@ struct GridImageItem: View {
   @Binding var selectedImageArray:[ImageModel]
   @Binding var imagesToShare:[ShareablePhoto]
 
-    var body: some View {
-      VStack {
-        ZStack {
-          theImage.imageFile
-            .resizable()
-            .aspectRatio(contentMode: .fit)
-            .frame(width:100, height: 100)
+  var body: some View {
+    VStack {
+      ZStack {
+        theImage.imageFile
+          .resizable()
+          .aspectRatio(contentMode: .fit)
+          .frame(width:100, height: 100)
+          .onTapGesture {
+            isSelected = !isSelected
+            selectedImageArray.append(theImage)
+            let newImageToAdd = ShareablePhoto(image: theImage.imageFile, caption: "Here's the caption")
+            imagesToShare.append(newImageToAdd)
+          }
+        if (isSelected) {
+          Image("GreenSelected")
+            .frame(width: 100, height: 100)
             .onTapGesture {
               isSelected = !isSelected
-              selectedImageArray.append(theImage)
-              let newImageToAdd = ShareablePhoto(image: theImage.imageFile, caption: "Here's the caption")
-              imagesToShare.append(newImageToAdd)
+              selectedImageArray.removeAll(where: { theImage.id == $0.id })
+              imagesToShare = []
+              imagesToShare = selectedImageArray.map({ ShareablePhoto(image: $0.imageFile, caption: "") })
             }
-          if (isSelected) {
-            Image("GreenSelected")
-              .frame(width: 100, height: 100)
-              .onTapGesture {
-                isSelected = !isSelected
-              }
-          }
         }
       }
     }
+  }
 }
 
 struct GridImageItem_Previews: PreviewProvider {
-    static var previews: some View {
-      let previewImage = ImageModel(id: UUID(), imageFile: Image("DropTarget"))
-      GridImageItem(theImage: previewImage, selectedImageArray: .constant([previewImage]), imagesToShare: .constant([]))
-    }
+  static var previews: some View {
+    let previewImage = ImageModel(id: UUID(), imageFile: Image("DropTarget"))
+    GridImageItem(theImage: previewImage, selectedImageArray: .constant([previewImage]), imagesToShare: .constant([]))
+  }
 }
